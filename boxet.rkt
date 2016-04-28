27adde09a87794e16a4cce994e42fc669457ed7b
theorangeone <kyleverhoog@gmail.com>
2016-04-28 15:30:51 -0400

    finish list-directory

--- boxet.rkt
+++ boxet.rkt
@@ -7,7 +7,8 @@
 
 (require "api-key.rkt"
          "json-builder.rkt")
-(provide list-directory)
+(provide list-directory
+         list-directory-los)
 
 
 (define api-base "https://api.dropboxapi.com/2/")
@@ -31,25 +32,38 @@
                        #:data data))
   (read-json resp))
 
-;; return listing of the directory given in json format
-(define (list-directory-jsexpr dir)
+;; (list-directory-jsexpr path recursive include_media_info include_deleted) -> jsexpr
+;; path: string
+;;   the path to the folder you want to see the contents of
+;; recursive: boolean
+;;   if #t, the list folder operation will be applied recursively to all subfolders and the response
+;;   will contain contents of all subfolders, the default for this field is #f
+;; include_media_info: boolean
+;;   if #t, FileMetadata.media_info is set for photo and video. The default for this field is #f.
+;; include_deleted: boolean
+;;   if #t, the results will include entries for files and folders that used to exist but were deleted.
+;;   the default for this field is #f.
+(define (list-directory-jsexpr path [recursive #f] [media_info #f] [incl_deleted #f])
   (define end-point "files/list_folder")
   (define method "POST")
-  ;(define data (string-append "{\"path\":\"" dir "\"}"))
-  (define data (build-json (make-json-obj (list (make-json-kv "path" dir)))))
+  (define data (build-json
+                (make-json-obj
+                 (list (make-json-kv "path" path)
+                       (make-json-kv "recursive" (if recursive json-true json-false))
+                       (make-json-kv "include_media_info" (if media_info json-true json-false))
+                       (make-json-kv "include_deleted" (if incl_deleted json-true json-false))))))
   (call-api end-point method data))
 
-(define (list-directory-json dir)
-  (jsexpr->string (list-directory-jsexpr dir)))
+(define (list-directory-json path)
+  (jsexpr->string (list-directory-jsexpr path)))
 
-
-(define (list-directory dir)
-  (define jsexpr (list-directory-jsexpr dir))
+;; returns a listified version of the json of the directory listing
+(define (list-directory path [rec #f] [mi #f] [id #f])
+  (define jsexpr (list-directory-jsexpr path rec mi id))
   (hash->strlist (safe-hash-ref jsexpr 'entries)))
 
-
-(define (boxet)
-  (list-directory ""))
-
-
-(boxet)
+;; returns a list of strings of the files and directories for the given path
+(define (list-directory-los path [rec #f] [mi #f] [id #f])
+  (filter (λ (x) (not (string=? x "name")))
+          (flatten (map (λ (x) (filter (λ (x) (equal? (first x) "name")) x))
+                        (list-directory path rec mi id)))))
